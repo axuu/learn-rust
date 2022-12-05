fe7fd9b840c835ff8030165d22fde18c1ade156e
axuu <zhangxu.fred@gmail.com>
2022-12-06 03:05:32 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:#?}", rect1);
+}
